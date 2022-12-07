532fd47b9aa647d9e41bef96d5ceb2c2772f55c9
Paul Maweu <paulmaweu09@gmail.com>
2022-12-07 09:11:01 +0300

    refacored the code

--- main.js
+++ main.js
@@ -1,37 +1,30 @@
+let homeStorEl = document.getElementById("home-score");
+let awayStoreEl = document.getElementById("away-score");
+let homeScore = 0;
+let awayScore = 0;
 
-let homeScoreEL=document.getElementById("home-score-1");
-let homeStorEl=document.getElementById("home-score")
-let awayScoreEl=document.getElementById("away-score-1")
-let awayStoreEl=document.getElementById("away-score")
-let homeScore=0;
-let awayScore=0;
-
-
-function increaseHomeScoreOne(){
-  homeScore +=1;
-  homeStorEl.textContent=homeScore 
+function increaseHomeScoreOne() {
+  homeScore += 1;
+  homeStorEl.textContent = homeScore;
 }
-function increaseHomeScoreTwo()
-{
-  homeScore +=2;
-  homeStorEl.textContent=homeScore 
+function increaseHomeScoreTwo() {
+  homeScore += 2;
+  homeStorEl.textContent = homeScore;
 }
 
-function increaseHomeScoreThree(){
-  homeScore +=3;
-  homeStorEl.textContent=homeScore 
+function increaseHomeScoreThree() {
+  homeScore += 3;
+  homeStorEl.textContent = homeScore;
 }
-function increaseAwayScoreOne(){
-  awayScore +=1;
-  awayStoreEl.textContent=awayScore 
-}function increaseAwayScoreTwo(){
-  awayScore +=2;
-  awayStoreEl.textContent=awayScore 
-}function increaseAwayScoreThree(){
-  awayScore +=3;
-  awayStoreEl.textContent=awayScore
+function increaseAwayScoreOne() {
+  awayScore += 1;
+  awayStoreEl.textContent = awayScore;
 }
-
-
-
-
+function increaseAwayScoreTwo() {
+  awayScore += 2;
+  awayStoreEl.textContent = awayScore;
+}
+function increaseAwayScoreThree() {
+  awayScore += 3;
+  awayStoreEl.textContent = awayScore;
+}
